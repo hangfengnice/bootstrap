86bb379519f426c614b9582941b5ff4fad52ad69
hangfengnice <nicehangfeng@foxmail.com>
2019-07-02 17:57:38 +0800

    city-search

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="search">
+   <input class='search-input' type="text" placeholder="输入城市名或拼音">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CitySearch"
+};
+</script>
+
+<style lang='scss' scoped>
+
+ .search{
+   touch-action: none;
+   height: .72rem;
+   padding: 0 .1rem;
+   background-color: #00bcd4;
+   .search-input{
+     box-sizing: border-box;
+     width: 100%;
+     height: .62rem;
+     line-height: .62rem;
+     text-align: center;
+     padding: 0 .1rem;
+     border-radius: .06rem;
+     color: #666; 
+   }
+ }
+</style>
