5fa6b1e9d3d4d9a6ec24eabb3760c0976789a37e
hangfengnice <nicehangfeng@foxmail.com>
2019-07-03 18:19:14 +0800

    city-keepalive
    activated  钩子函数

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -16,6 +16,7 @@ import HomeIcons from "./components/Icons";
 import HomeRecommend from "./components/Recommend";
 import HomeWeekend from "./components/Weekend";
 import axios from "axios";
+import { mapState } from "vuex";
 
 export default {
   name: "Home",
@@ -29,15 +30,21 @@ export default {
   data() {
     return {
       // city: "",
+      lastCity: "",
       swiperList: [],
       iconList: [],
       recommendList: [],
       weekendList: []
     };
   },
+  computed: {
+    ...mapState(["city"])
+  },
   methods: {
     getHomeInfo() {
-      axios.get("/api/mock/index.json").then(this.getHomeInfoSucc);
+      axios
+        .get("/api/mock/index.json?city=" + this.city)
+        .then(this.getHomeInfoSucc);
     },
     getHomeInfoSucc(res) {
       res = res.data;
@@ -52,7 +59,14 @@ export default {
     }
   },
   mounted() {
+    this.lastCity = this.city;
     this.getHomeInfo();
+  },
+  activated() {
+    if ((this.lastCity = this.city)) {
+      this.lastCity = this.city;
+      this.getHomeInfo();
+    }
   }
 };
 </script>
